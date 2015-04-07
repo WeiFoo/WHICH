94da96c9c96474b526692a768e44aecd2a968571
WeiFoo <wfu@ncsu.edu>
2015-04-06 21:52:08 -0400

    CART plots done!

--- which.py
+++ which.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
-def csv(f= "./data/ant/ant-1.5copy.csv"):
+def csv(f= "./data/ant/ant-1.7copy.csv"):
   t = table(f)
   n = [ header.name for header in t.headers]
   d = sorted([ row.cells for row in t._rows],key =lambda z: z[10] ) # sorted by $loc
@@ -54,13 +54,32 @@ def gbest(t):
   data = sorted(data, key = lambda z: z[10])
   return XY(data)
 
-def cart(t):
-  clf = DecisionTreeRegressor(random_state = 1)
-  clf.fit()
+def cart(train,test):
+  TP,Loc = 0,0
+  x,pd = [],[]
+  train_x =[ t.cells[:-1]for t in train.data]
+  train_y = [(t.cells[-1]) for t in train.data]
+  test_x = [t.cells[:-1] for t in test.data]
+  test_y = [(t.cells[-1]) for t in test.data]
+  clf = DecisionTreeRegressor(random_state = 1).fit(train_x,train_y)
+  array = clf.predict(test_x)
+  predictresult = [i for i in array]
+  for i,d in enumerate(test.data):
+    if d.cells[-1] == predictresult[i] and predictresult[i] ==1:
+      TP+=1
+    Loc += d.cells[10]
+    x +=[100*Loc/the.effortNorm.Total[10]]
+    pd +=[100*TP/the.NP.defective]
+  x = np.array(x)
+  pd = np.array(pd)
+  pdb.set_trace()
+  return[x,pd]
+
+
 
 def plot(result):
-  color = ['r-','b-','k-','g-','y-']
-  labels = ['manualUp','manualDown','minimum','good','best']
+  color = ['r-','b-','k-','g-','y-','c-']
+  labels = ['manualUp','manualDown','minimum','good','best','CART']
   plt.figure(1)
   # plt.figure(figsize=(8,4))
   for j,x in enumerate(result):
@@ -69,34 +88,33 @@ def plot(result):
   plt.ylabel("PD(% probability of detection)")
   plt.title("Effort-vs-PD")
   plt.ylim(0,100)
-  plt.legend(loc='down right')
+  plt.legend(loc='best')
   # plt.text(60,20,'manualDown')
   # plt.text(35,70,'manualUp')
   plt.show()
   pdb.set_trace()
 
-def _rule():
+def _rule(train):
   LIB(seed=1)
   RULER(tiny=4,better=gt)
-  def _ruler():
-    train=csv()
-    print(train.score, "baseline :",len(train.data))
-    for z in ruler(train):
-      print(z.score,z)
-    best = ruler(train)[0]
-    # actual,predict = best.predict(csv())
-    # _Abcd(predict, actual)
-    return best
-  return _ruler()
+  print(train.score, "baseline :",len(train.data))
+  for z in ruler(train):
+    print(z.score,z)
+  best = ruler(train)[0]
+  # actual,predict = best.predict(csv())
+  # _Abcd(predict, actual)
+  return best
 
 def _main():
-  bestrule=_rule()
-  test = csv(f= "./data/ant/ant-1.4copy.csv")
+  train=csv()
+  bestrule=_rule(train)
+  test = csv(f= "./data/ant/ant-1.6copy.csv")
   result=[manual(test)]
   result +=[manual(test,up = False)]
   result +=[[np.linspace(0,100,100),np.linspace(0,100,100)]]
   result +=[bestrule.predict(test)]
   result +=[gbest(test)]
+  result +=[cart(train,test)]
   plot(result)
 
 if __name__ == "__main__":
