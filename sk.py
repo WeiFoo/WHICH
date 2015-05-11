70764a951ce756b8ce58eba75215cf5bb75c9232
WeiFoo <wfu@ncsu.edu>
2015-05-10 22:38:50 -0400

    include sk.py

--- sk.py
+++ sk.py
@@ -471,12 +471,6 @@ def leftRight(parts,epsilon=0.01):
 Driver for the demos:
 
 """
-def writefile(s):
-  global The
-  f = open(The.option.resultname, 'a')
-  f.write(s+'\n')
-  f.close()
-
 def rdivDemo(data):
   def z(x):
     return int(100 * (x - lo) / (hi - lo + 0.00001))
@@ -494,16 +488,11 @@ def rdivDemo(data):
   last = None
   print  ('%4s , %20s ,    %s   , %4s ' % \
                ('rank', 'name', 'med', 'iqr'))+ "\n"+ line
-  writefile(('%4s , %20s ,    %s   , %4s ' % \
-               ('rank', 'name', 'med', 'iqr'))+ "\n"+ line)
   for _,__,x in sorted(ranks):
     q1,q2,q3 = x.quartiles()
     print  ('%4s , %20s ,    %4s  ,  %4s ' % \
                  (x.rank+1, x.name, q2, q3 - q1))  + \
               xtile(x.all,lo=lo,hi=hi,width=30,show="%5.2f")
-    writefile(('%4s , %20s ,    %4s  ,  %4s ' % \
-                 (x.rank+1, x.name, q2, q3 - q1))  + \
-              xtile(x.all,lo=lo,hi=hi,width=30,show="%5.2f"))
     last = x.rank 
 """
 
